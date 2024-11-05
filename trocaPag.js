550de042fbfd64b590bcf2b9782002af3e9cd242
GabrielRibeiro1503 <gabrielribeiro1503.rs@gmail.com>
2024-11-04 22:25:11 -0300

    Estruturação das paginas de drop

---
+++ trocaPag.js
@@ -0,0 +1,86 @@
+const conteudos = {
+    raro: '<img src="assets/Drops/Coin.png" alt=""><img src="assets/Drops/PP.png" alt=""><img src="assets/Drops/XpDouble.png" alt=""><img src="assets/Drops/Vales.png" alt="" id="vales"><img src="assets/Drops/Bling.png" alt="">',
+    superraro: '<img src="assets/Drops/Coin.png" alt=""><img src="assets/Drops/PP.png" alt=""><img src="assets/Drops/XpDouble.png" alt=""><img src="assets/Drops/Vales.png" alt="" id="vales"><img src="assets/Drops/Bling.png" alt=""><img src="assets/Drops/CommonPin.png" alt="" class="pin"><img src="assets/Drops/Spray.png" alt="" id="spray">',
+    epico: '<img src="assets/Drops/Coin.png" alt=""><img src="assets/Drops/PP.png" alt=""><img src="assets/Drops/XpDouble.png" alt=""><img src="assets/Drops/CommonPin.png" alt="" class="pin"><img src="assets/Drops/Spray.png" alt="" id="spray"><img src="assets/Drops/RareBrawler.png" alt=""><img src="assets/Drops/RarePin.png" alt="" class="pin"><img src="assets/Drops/RareSkin.png" alt="">',
+    mitico: `
+        <div class="titulo">
+            <h2>MÍTICO</h2>
+        </div>
+        <div class="container">
+            <div class="sections" id="visual">
+                <h2>VISUAL</h2>
+                <div class="partes">
+                    <div class="icon-container">
+                        <div class="textosup"><p></p></div>
+                        <img src="assets/Drops/Spray.png" alt="" id="spray">
+                        <div class="textoinf"><p>6,33%</p></div>
+                    </div>
+                    <div class="icon-container">
+                        <div class="textosup"><p></p></div>
+                        <img src="assets/Drops/RarePin.png" alt="" class="pin">
+                        <div class="textoinf"><p>6,33%</p></div>
+                    </div>
+                    <div class="icon-container">
+                        <div class="textosup"><p></p></div>
+                        <img src="assets/Drops/RareSkin.png" alt="">
+                        <div class="textoinf"><p>15,82%</p></div>
+                    </div>
+                </div>
+                <div class="partes">
+                    <div class="icon-container">
+                        <div class="textosup"><p></p></div>
+                        <img src="assets/Drops/ProfileIcon.png" alt="">
+                        <div class="textoinf"><p>6,33%</p></div>
+                    </div>
+                    <div class="icon-container">
+                        <div class="textosup"><p></p></div>
+                        <img src="assets/Drops/EpicPin.png" alt="" class="pin">
+                        <div class="textoinf"><p>3,16%</p></div>
+                    </div>
+                </div>
+            </div>
+            <div class="sections">
+                <h2>BRAWLERS</h2>
+                <div class="icon-container">
+                    <div class="textosup"><p></p></div>
+                    <img src="assets/Drops/EpicBrawler.png" alt="">
+                    <div class="textoinf"><p>6,33%</p></div>
+                </div>
+                <div class="icon-container">
+                    <div class="textosup"><p></p></div>
+                    <img src="assets/Drops/MythicBrawler.png" alt="">
+                    <div class="textoinf"><p>1,90%</p></div>
+                </div>
+                <div class="icon-container">
+                    <div class="textosup"><p></p></div>
+                    <img src="assets/Drops/SuperRareBrawler.png" alt="">
+                    <div class="textoinf"><p>9,49%</p></div>
+                </div>
+            </div>
+            <div class="baixo">
+                <div class="sections" style="width: 160px;">
+                    <h2 style="left: 26px;">RECURSOS</h2>
+                    <div class="icon-container">
+                        <div class="textosup"><p>500</p></div>
+                        <img src="assets/Drops/Coin.png" alt="">
+                        <div class="textoinf"><p>9,49%</p></div>
+                    </div>
+                    <div class="icon-container">
+                        <div class="textosup"><p>200</p></div>
+                        <img src="assets/Drops/PP.png" alt="">
+                        <div class="textoinf"><p>18,99%</p></div>
+                    </div>
+                </div>
+                <div class="sections" style="width: 130px;">
+                    <h2 style="left: 17px;">GADGETS</h2>
+                    <div class="icon-container">
+                        <div class="textosup"><p></p></div>
+                        <img src="assets/Acessorios/gadget.png" alt="">
+                        <div class="textoinf"><p>15,82%</p></div>
+                    </div>
+                </div>
+            </div>    
+        </div>
+        `,
+    lendario: '<img src="assets/Acessorios/hyper.png" id="hyper" alt=""><img src="assets/Acessorios/starpower.png" alt=""><img src="assets/Drops/EpicBrawler.png" alt=""><img src="assets/Drops/MythicBrawler.png" alt=""><img src="assets/Drops/LegendaryBrawler.png" alt=""><img src="assets/Drops/SuperRareSkin.png" alt=""><img src="assets/Drops/EpicSkin.png" alt="">'
+}
